19ab4ee21a4d46460bacf14825f93365ac225eaf
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-23 19:36:32 +0100

    Update comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 /* Search
  * A simple grep like tool for searching text in files
  * Github: https://www.github.com/awesomelewis2007/search
- * License: GPL-3.0
+ * Licence: GPL-3.0
  * By: Lewis Evans
 */
 
